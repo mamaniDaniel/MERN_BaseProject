c5568f1cff2d40e48e21a94c48f187208fddc764
daniel mamani <dani.oscar.257@gmail.com>
2020-06-09 02:14:06 -0300

    OK: prueba fetch a backend, listo para primer deploy en heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getUsers(setUsers, setError){
+  fetch('/api/users')
+    .then((res)=> res.json())
+    .then((json)=> setUsers(json))
+    .catch((err)=>{ console.log("error fetch api/users: ", err); setError(err)})
+}
+
 function App() {
+  const [users, setUsers]= useState([]);
+  const [errors, setError]= useState(null);
+
+  useEffect( ()=>{
+    getUsers(setUsers, setError)
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +30,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+        {errors ? <p>Error</p> : null}
+        {users.map((user)=>
+          <p key={user._id}> {user.name}</p>  
+        )}
           Learn React
         </a>
       </header>
